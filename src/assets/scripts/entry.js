54c64a23feec70990ce11eedf6f343b29bca4ad8
Wesley Smith <me@wesjd.net>
2019-05-25 18:53:08 -0400

    literaly render latex

--- src/assets/scripts/entry.js
+++ src/assets/scripts/entry.js
@@ -13,17 +13,15 @@ window.addEventListener("DOMContentLoaded", () => {
     staticmath.config({
         handlers: {
             edit: () => {
-                const values = staticmath.innerFields.map(field => parseInt(field.latex()))
-                if (!values.some(value => isNaN(value))) {
-                    const points = renderer.points = []
-                    defaultPoints.forEach(point => points.push(multiply(values, point)))
-                    renderer.render()
-                }
+                const values = staticmath.innerFields.map(field => field.latex())
+                const points = renderer.points = []
+                defaultPoints.forEach(point => points.push(multiply(values, point)))
+                renderer.render()
             }
         }
     })
 })
 
 function multiply(matrix, point) {
-    return [((matrix[0] * point[0]) + (matrix[1] * point[1])), ((matrix[2] * point[0]) + (matrix[3] * point[1]))]
+    return [`(${matrix[0]} * ${point[0]}) + (${matrix[1]} * ${point[1]})`, `(${matrix[2]} * ${point[0]}) + (${matrix[3]} * ${point[1]})`]
 }
